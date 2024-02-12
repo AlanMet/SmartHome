6ba8f7ace5a3e4dae14edaca15f844e8e8093daa
MysteryCrystal <106497267+MysteryCrystal@users.noreply.github.com>
2024-02-12 22:33:08 +0000

    Inheritance and smart home

--- backend.py
+++ backend.py
@@ -1,17 +1,22 @@
-class SmartPlug:
+class SmartDevice:
+    def __init__(self):
+        self.switchedOn = False
+
+    def toggleSwitch(self):
+        if self.switchedOn == True:
+            self.switchedOn = False
+        else:
+            self.switchedOn = True
+
+    def getSwitchedOn(self):
+        return self.switchedOn
+
+
+class SmartPlug(SmartDevice):
     def __init__(self, rate):
-        self.switchedOn = False
+        super.__init__()
         self.consumptionRate = rate
 
-    def toggleSwitch(self):
-        if self.switchedOn == True:
-            self.switchedOn = False
-        else:
-            self.switchedOn = True
-
-    def getSwitchedOn(self):
-        return self.switchedOn
-    
     def getConsumptionRate(self):
         return self.consumptionRate
     
@@ -24,39 +29,60 @@ class SmartPlug:
             switch = "on"
 
         return f"plug status: switched {switch}\nconsumption rate: {self.consumptionRate}"
-    
 
-class SmartFridge:
-    temps = {
-        3, 4, 7
-    }
-    def __init__(self, temperature):
-        self.switchedOn = False
-        if temperature in temps:
-            self.temperature = temperature
 
-    def toggleSwitch(self):
-        if self.switchedOn == True:
-            self.switchedOn = False
-        else:
-            self.switchedOn = True
+class SmartFridge(SmartDevice):
+    def __init__(self):
+        super().__init__()
+        #default value from the document
+        self.temperature = 3
 
-    def getSwitchedOn(self):
-        return self.switchedOn
+    def getTemperature(self):
+        return self.temperature
     
-    def getConsumptionRate(self):
-        return self.consumptionRate
-    
-    def setConsumptionRate(self, rate):
-        self.consumptionRate = rate
+    def setTemperature(self, temp):
+        temps = [3, 4, 7]
+        if temp in temps:
+            self.temperature = temp
 
     def __str__(self):
         switch = "off"
         if self.switchedOn == True:
             switch = "on"
 
-        return f"plug status: switched {switch}\nconsumption rate: {self.consumptionRate}"
+        return f"plug status: switched {switch}\nconsumption rate: {self.temp}"
     
+
+class SmartHome:
+    def __init__(self):
+        self.devices = []
+
+    def getDevices(self):
+        return self.devices
+    
+    def getDeviceAt(self, index):
+        #if starting from 1 [index-1]
+        return self.devices[index]
+    
+    def addDevice(self, device):
+        self.devices.append(device)
+
+    def toggleSwitch(self, index):
+        self.devices[index].toggleSwitch()
+
+    def turnOnAll(self):
+        for device in self.devices:
+            if not device.getSwitchedOn():
+                device.toggleSwitch()
+
+    def turnOffAll(self):
+        for device in self.devices:
+            if device.getSwitchedOn():
+                device.toggleSwitch()
+
+    def __str__(self):
+        pass
+
 def testSmartPlug():
     plug = SmartPlug(45)
     plug.toggleSwitch()
@@ -65,4 +91,5 @@ def testSmartPlug():
     plug.setConsumptionRate(50)
     print(plug.getConsumptionRate())
     print(plug)
+
 
